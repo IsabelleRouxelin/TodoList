50821d96233349fd880c4fc15be1f45806303322
IsabelleRouxelin <isabelle.rouxelin@gmail.com>
2025-06-28 00:05:08 +0100

    added DOM elements, event listener and began adusting css styling and html input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,40 @@
-// To do list
+//Todo List
+// Thing we need:
+/* arrays to store taks 
+event listeners to respond to clicks to create/remove tasks *maybe edit
+need to be able to add and delete tasks to start 
+*/
+// Array to store tasks 
+var tasks = [];
+var newTask = [1];
 
-//Array to store tasks 
+//DOM elements
+var taskInput = document.getElementById("taskInput");
+var addBtn = document.getElementById("addBtn");
+var errorMsg = document.getElementById("errorMsg");
+var todoList = document.getElementById("todoList");
+var emptyMsg = document.getElementById("emptyMsg");
 
-//Event listeners
+// Event listeners
+//add task 
+addBtn.addEventListener("click", addTask);
 
-//input
+//validate input
+function addTask() {
+    var text = taskInput.value(); //gets task input 
+    
+    if (text === "") {
+        showError("Please enter a task"); // if empty show error
+        return;
+    }
+}
 
-//loop through array and create elements
+// display and update tasks
 
-//display tasks
+// should we use push? or should this be
+
+//implement the delete functionality
+
+// *tasks to be edited 
+
+//* tasks to be saved
